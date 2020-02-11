02ac90dd3db6bb6146c1649d0415d7b1f96e57ed
Roger Robertson Jr <rjrjr@pm.me>
2020-02-11 23:17:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -228,5 +228,20 @@ def big_shoe_rebounds
   rebounds
 end
 
+def most_points_scored
+  
+end
+
+def winning_team
+  
+end
+
+def player_with_longest_name
+  
+end
+  
+def long_name_steals_a_ton
+  
+end
 
 
